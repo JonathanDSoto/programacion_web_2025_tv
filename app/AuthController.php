9d107f971e5bda523c4124f0a33f9767e8cd0661
Jonathan Soto <jonathansoto@Jonathans-MacBook-Air.local>
2025-10-29 17:05:53 -0700

    login

--- app/AuthController.php
+++ app/AuthController.php
@@ -1,17 +1,57 @@
 <?php 
+	#ini_set('display_errors', 1);
+	#ini_set('display_startup_errors', 1);
+	#error_reporting(E_ALL); 
 
+	include "connectionController.php";
 
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+	$action = $_POST['action'];
 
+	if ($action == "login") {
 
-	if ($username == "jonathansoto") {
-		
-		if ($password == "123123123") { 
+	 	$username = $_POST['username'];
+		$password = $_POST['password'];	
+
+		$auth = new AuthController();
+		$auth->login($username,$password);
+	}  
+
+class AuthController{ 
+
+	private $connection;
+
+	public function __construct() {
+	    $this->connection = new ConnectionController();
+	}
+
+	function login($username,$password)
+	{
+
+		$conn = $this->connection->connect();
+		if (!$conn->connect_error) {
 			
-			header('Location: ../home.html');
-		}
-	}else
-		header('Location: ../login.html');
+			$query = "select * from users where email = ? and password = ?";
+
+			$prepared_query = $conn->prepare($query);
+
+			$prepared_query->bind_param('ss', $username, $password);
+
+			$prepared_query->execute();
+
+			$results = $prepared_query->get_result();
+			$users = $results->fetch_all(MYSQLI_ASSOC);
+
+			if (count($users)>0) {
+				
+				header('Location: ../home.html');
+
+			}else
+				header('Location: ../login.html');
+			
+		}else
+			header('Location: ../login.html');
+	} 
+
+}
 
 ?>
