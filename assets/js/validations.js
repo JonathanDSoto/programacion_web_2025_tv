bf817b592cbf37efbdde8f178458c196743492e5
Jonathan Soto <jsoto1429@gmail.com>
2025-11-21 15:25:13 -0700

    validaciones de front

--- assets/js/validations.js
+++ assets/js/validations.js
@@ -1,11 +1,12 @@
-function onlyLetters(event)
-{
-	const pressedKey = event.key;
-	const letters = ["a", "á", "A","Á","e"];
-
-	if(!letters.includes(pressedKey)){
-	  	return false
-	}
-
-	//validate javascript regex email
+function soloLetras(e) {
+    var t = e.key.toLowerCase();
+    return /^[a-zñáéíóúü\s]$/i.test(t);
+}
+function soloNumeros(e) {
+    var t = e.key.toLowerCase();
+    return /^[0-9.]$/.test(t);
+}
+function soloLetrasYnumeros(e) {
+    var t = e.key.toLowerCase();
+    return /^[a-zñáéíóúü\s0-9.]+$/.test(t);
 }
